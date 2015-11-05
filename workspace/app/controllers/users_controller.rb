33df4aa634f30c13bd281df6e3cd51f4d8b62a6d
verdu103 <verdu103@mail.chapman.edu>
2015-11-04 16:52:44 -0800

    update
    update

--- workspace/app/controllers/users_controller.rb
+++ workspace/app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    ## Not the final implementation
+    @user = User.new(user_params)   
+    if @user.save
+      flash[:success] = "Welcome to the Zen Blog"
+      redirect_to @user
+      ##create users/create.html.erb to make this work!! holy shit! that sucked.
+      #redirect_to :action => :index #Handle a successful save -> change this later.
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+      def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 end
